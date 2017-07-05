cce6853f55fbc7feba818c018388b55c1b6fbe48
meZS <798506095@qq.com>
2017-07-05 15:43:54 +0800

    Initial Commit

---
+++ IOSGCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSGCD
+//
+//  Created by wordy on 2017/7/5.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
